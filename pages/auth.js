56250537225917f3ff7dc822add48669ed7210f1
Jennifer Arnold <cannaestia@gmail.com>
2023-05-09 12:05:11 -0400

    updated linkedin

---
+++ pages/auth.js
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
+const clientId = process.env.NEXT_PUBLIC_LINKEDIN_CLIENT_ID;
+const clientSecret = process.env.LINKEDIN_CLIENT_SECRET;
+const redirectUri = 'http://localhost:3000/auth';
+
+async function getAccessToken(code) {
+  const tokenUrl = 'https://www.linkedin.com/oauth/v2/accessToken';
+  const response = await axios.post(tokenUrl, null, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    params: {
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: redirectUri,
+      client_id: clientId,
+      client_secret: clientSecret,
+    },
+  });
+
+  return response.data.access_token;
+}
+
+export default function AuthPage() {
+  const router = useRouter();
+  const { code } = router.query;
+
+  useEffect(() => {
+    if (code) {
+      getAccessToken(code)
+        .then((accessToken) => {
+          // Here you can handle the access token. For example, you could store it in
+          // local storage and redirect the user to another page.
+          localStorage.setItem('linkedin_access_token', accessToken);
+          router.push('/');
+        })
+        .catch((error) => {
+          console.error(error);
+          // Here you could handle the error, for example by showing an error message to the user.
+        });
+    }
+  }, [code, router]);
+
+  return <div>Loading...</div>;
+}
--- pages/linkedin.js
+++ pages/linkedin.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React from "react";
+import Head from "next/head";
 
 const clientId = process.env.NEXT_PUBLIC_LINKEDIN_CLIENT_ID;
 const redirectUri = 'http://localhost:3000/auth';
 const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=r_liteprofile`;
 
-function Linkedin() {
+export default function Linkedin() {
   return (
     <div>
-      <h1>LinkedIn Scraper</h1>
-      <a href={authUrl}>Connect with LinkedIn</a>
+      <Head>
+        <title>LinkedIn Scraper</title>
+        <meta name="description" content="LinkedIn Scraper" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>LinkedIn Scraper</h1>
+        <a href={authUrl}>Connect with LinkedIn</a>
+      </main>
     </div>
   );
 }
 
-export default Linkedin;
